e56f704a4d3ed1be524258c96b50d1880f6bc851
By1337 <by133700@gmail.com>
2024-11-22 21:00:38 +0300

    feat: camel

---
+++ api/src/main/java/dev/by1337/virtualentity/api/virtual/animal/camel/VirtualCamel.java
@@ -0,0 +1,11 @@
+package dev.by1337.virtualentity.api.virtual.animal.camel;
+
+public interface VirtualCamel extends dev.by1337.virtualentity.api.virtual.animal.horse.VirtualAbstractHorse {
+    boolean isDashing();
+
+    void setDashing(boolean flag);
+
+    long getLastPoseChangeTick();
+
+    void setLastPoseChangeTick(long tick);
+}
--- core/src/main/java/dev/by1337/virtualentity/core/mappings/VirtualEntityRegistrar.java
+++ core/src/main/java/dev/by1337/virtualentity/core/mappings/VirtualEntityRegistrar.java
@@ -11,6 +11,7 @@ import dev.by1337.virtualentity.core.virtual.animal.axolotl.*;
 import dev.by1337.virtualentity.core.virtual.animal.goat.*;
 import dev.by1337.virtualentity.core.virtual.animal.horse.*;
 import dev.by1337.virtualentity.core.virtual.animal.frog.*;
+import dev.by1337.virtualentity.core.virtual.animal.camel.*;
 import dev.by1337.virtualentity.core.virtual.decoration.VirtualArmorStandImpl;
 import dev.by1337.virtualentity.core.virtual.display.*;
 import dev.by1337.virtualentity.core.virtual.item.*;
@@ -151,5 +152,6 @@ public class VirtualEntityRegistrar {
         factory.register(VirtualEntityType.SNIFFER, VirtualSnifferImpl::new, Version.V1_19_4);
         factory.register(VirtualEntityType.TADPOLE, VirtualTadpoleImpl::new, Version.V1_19_4);
         factory.register(VirtualEntityType.FROG, VirtualFrogImpl::new, Version.V1_19_4);
+        factory.register(VirtualEntityType.CAMEL, VirtualCamelImpl::new, Version.V1_19_4);
     }
 }
