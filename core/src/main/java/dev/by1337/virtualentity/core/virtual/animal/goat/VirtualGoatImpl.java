b90b3395b19e46983b70fc4f1aa973ba6661a563
By1337 <by133700@gmail.com>
2024-11-22 20:22:51 +0300

    feat: port goat to 1.19.4

--- core/src/main/java/dev/by1337/virtualentity/core/virtual/animal/goat/VirtualGoatImpl.java
+++ core/src/main/java/dev/by1337/virtualentity/core/virtual/animal/goat/VirtualGoatImpl.java
@@ -5,10 +5,15 @@ import dev.by1337.virtualentity.api.entity.VirtualEntityType;
 import dev.by1337.virtualentity.core.mappings.Mappings;
 import dev.by1337.virtualentity.core.syncher.EntityDataAccessor;
 import dev.by1337.virtualentity.core.virtual.VirtualAgeableMobImpl;
+import org.by1337.blib.util.Version;
 
 @SinceMinecraftVersion("1.17.1")
 public class VirtualGoatImpl extends VirtualAgeableMobImpl implements dev.by1337.virtualentity.api.virtual.animal.goat.VirtualGoat {
     private static final EntityDataAccessor<Boolean> DATA_IS_SCREAMING_GOAT;
+    @SinceMinecraftVersion("1.19.4")
+    private static final EntityDataAccessor<Boolean> DATA_HAS_LEFT_HORN;
+    @SinceMinecraftVersion("1.19.4")
+    private static final EntityDataAccessor<Boolean> DATA_HAS_RIGHT_HORN;
 
     public VirtualGoatImpl() {
         super(VirtualEntityType.GOAT);
@@ -17,6 +22,10 @@ public class VirtualGoatImpl extends VirtualAgeableMobImpl implements dev.by1337
     protected void defineSynchedData() {
         super.defineSynchedData();
         this.entityData.define(DATA_IS_SCREAMING_GOAT, false);
+        if (Version.VERSION.newerThanOrEqual(Version.V1_19_4)) {
+            entityData.define(DATA_HAS_LEFT_HORN, true);
+            entityData.define(DATA_HAS_RIGHT_HORN, true);
+        }
     }
 
     @Override
@@ -29,7 +38,39 @@ public class VirtualGoatImpl extends VirtualAgeableMobImpl implements dev.by1337
         this.entityData.set(DATA_IS_SCREAMING_GOAT, flag);
     }
 
+    @SinceMinecraftVersion("1.19.4")
+    @Override
+    public void setHasLeftHorn(boolean flag) {
+        entityData.set(DATA_HAS_LEFT_HORN, flag);
+    }
+
+    @SinceMinecraftVersion("1.19.4")
+    @Override
+    public boolean hasLeftHorn() {
+        return entityData.get(DATA_HAS_LEFT_HORN);
+    }
+
+    @SinceMinecraftVersion("1.19.4")
+    @Override
+    public void setHasRightHorn(boolean flag) {
+        entityData.set(DATA_HAS_RIGHT_HORN, flag);
+    }
+
+    @SinceMinecraftVersion("1.19.4")
+    @Override
+    public boolean hasRightHorn() {
+        return entityData.get(DATA_HAS_RIGHT_HORN);
+    }
+
     static {
         DATA_IS_SCREAMING_GOAT = Mappings.findAccessor("Goat", "DATA_IS_SCREAMING_GOAT");
+        if (Version.VERSION.newerThanOrEqual(Version.V1_19_4)) {
+            DATA_HAS_LEFT_HORN = Mappings.findAccessor("Goat", "DATA_HAS_LEFT_HORN");
+            DATA_HAS_RIGHT_HORN = Mappings.findAccessor("Goat", "DATA_HAS_RIGHT_HORN");
+        } else {
+            DATA_HAS_LEFT_HORN = null;
+            DATA_HAS_RIGHT_HORN = null;
+        }
+
     }
 }
