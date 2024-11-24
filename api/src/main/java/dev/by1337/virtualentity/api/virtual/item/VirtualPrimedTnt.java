1a676885f8c2f9aea3d3df28386bc6ecb4c89c19
By1337 <by133700@gmail.com>
2024-11-25 02:30:27 +0300

    feat: adapt VirtualPrimedTnt

--- api/src/main/java/dev/by1337/virtualentity/api/virtual/item/VirtualPrimedTnt.java
+++ api/src/main/java/dev/by1337/virtualentity/api/virtual/item/VirtualPrimedTnt.java
@@ -1,9 +1,17 @@
 package dev.by1337.virtualentity.api.virtual.item;
 
+import dev.by1337.virtualentity.api.annotations.SinceMinecraftVersion;
 import dev.by1337.virtualentity.api.virtual.VirtualEntity;
+import org.bukkit.block.data.BlockData;
 
 public interface VirtualPrimedTnt extends VirtualEntity {
     int getFuse();
 
     void setFuse(int fuse);
+
+    @SinceMinecraftVersion("1.20.4")
+    void setBlockState(BlockData block);
+
+    @SinceMinecraftVersion("1.20.4")
+    BlockData getBlockState();
 }
--- core/src/main/java/dev/by1337/virtualentity/core/virtual/item/VirtualPrimedTntImpl.java
+++ core/src/main/java/dev/by1337/virtualentity/core/virtual/item/VirtualPrimedTntImpl.java
@@ -1,12 +1,20 @@
 package dev.by1337.virtualentity.core.virtual.item;
 
+import dev.by1337.virtualentity.api.annotations.SinceMinecraftVersion;
 import dev.by1337.virtualentity.api.entity.VirtualEntityType;
 import dev.by1337.virtualentity.core.mappings.Mappings;
 import dev.by1337.virtualentity.core.syncher.EntityDataAccessor;
 import dev.by1337.virtualentity.core.virtual.VirtualEntityImpl;
+import org.bukkit.Material;
+import org.bukkit.block.data.BlockData;
+import org.by1337.blib.util.Version;
 
 public class VirtualPrimedTntImpl extends VirtualEntityImpl implements dev.by1337.virtualentity.api.virtual.item.VirtualPrimedTnt {
     private static final EntityDataAccessor<Integer> DATA_FUSE_ID;
+    @SinceMinecraftVersion("1.20.4")
+    private static final EntityDataAccessor<BlockData> DATA_BLOCK_STATE_ID;
+    private static final BlockData TNT = Material.TNT.createBlockData();
+
 
     public VirtualPrimedTntImpl() {
         super(VirtualEntityType.TNT);
@@ -15,6 +23,9 @@ public class VirtualPrimedTntImpl extends VirtualEntityImpl implements dev.by133
     protected void defineSynchedData() {
         super.defineSynchedData();
         this.entityData.define(DATA_FUSE_ID, 80);
+        if (Version.VERSION.newerThanOrEqual(Version.V1_20_4)) {
+            this.entityData.define(DATA_BLOCK_STATE_ID, TNT);
+        }
     }
 
     @Override
@@ -27,7 +38,22 @@ public class VirtualPrimedTntImpl extends VirtualEntityImpl implements dev.by133
         entityData.set(DATA_FUSE_ID, fuse);
     }
 
+    @SinceMinecraftVersion("1.20.4")
+    public void setBlockState(BlockData block) {
+        this.entityData.set(DATA_BLOCK_STATE_ID, block);
+    }
+
+    @SinceMinecraftVersion("1.20.4")
+    public BlockData getBlockState() {
+        return this.entityData.get(DATA_BLOCK_STATE_ID);
+    }
+
     static {
         DATA_FUSE_ID = Mappings.findAccessor("PrimedTnt", "DATA_FUSE_ID");
+        if (Version.VERSION.newerThanOrEqual(Version.V1_20_4)) {
+            DATA_BLOCK_STATE_ID = Mappings.findAccessor("PrimedTnt", "DATA_BLOCK_STATE_ID");
+        } else {
+            DATA_BLOCK_STATE_ID = null;
+        }
     }
 }
