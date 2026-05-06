d40ea3088c8ec196becf670bbd03a15871681a3e
by1337 <by133700@gmail.com>
2026-05-06 21:59:52 +0300

    fix: set_entity_motion packet

--- core/src/main/java/dev/by1337/virtualentity/core/network/impl/SetEntityMotionPacket.java
+++ core/src/main/java/dev/by1337/virtualentity/core/network/impl/SetEntityMotionPacket.java
@@ -1,9 +1,9 @@
 package dev.by1337.virtualentity.core.network.impl;
 
+import dev.by1337.core.ServerVersion;
 import dev.by1337.virtualentity.core.mappings.Packets;
 import dev.by1337.virtualentity.core.network.ByteBufUtil;
 import dev.by1337.virtualentity.core.network.Packet;
-import dev.by1337.virtualentity.core.network.PacketType;
 import io.netty.buffer.ByteBuf;
 import org.by1337.blib.geom.Vec3d;
 
@@ -22,9 +22,13 @@ public class SetEntityMotionPacket extends Packet {
     public void write(ByteBuf byteBuf) {
         ByteBufUtil.writeVarInt(PACKET_ID, byteBuf);
         ByteBufUtil.writeVarInt(id, byteBuf);
-        byteBuf.writeShort(toUnit(clamp(motion.x, -3.9, 3.9)));
-        byteBuf.writeShort(toUnit(clamp(motion.y, -3.9, 3.9)));
-        byteBuf.writeShort(toUnit(clamp(motion.z, -3.9, 3.9)));
+        if (ServerVersion.CURRENT_PROTOCOL >= ServerVersion.Protocol.V1_21_9) {
+            writeLpVec3(byteBuf, motion);
+        } else {
+            byteBuf.writeShort(toUnit(clamp(motion.x, -3.9, 3.9)));
+            byteBuf.writeShort(toUnit(clamp(motion.y, -3.9, 3.9)));
+            byteBuf.writeShort(toUnit(clamp(motion.z, -3.9, 3.9)));
+        }
     }
 
     private static short toUnit(double d) {
@@ -33,6 +37,47 @@ public class SetEntityMotionPacket extends Packet {
 
     public static double clamp(double source, double min, double max) {
         return source < min ? min : Math.min(source, max);
+    }
+
+    public static void writeLpVec3(ByteBuf buffer, Vec3d vector) {
+        double d = sanitize(vector.x);
+        double d1 = sanitize(vector.y);
+        double d2 = sanitize(vector.z);
+        double max = absMax(d, absMax(d1, d2));
+        if (max < 3.051944088384301E-5) {
+            buffer.writeByte(0);
+        } else {
+            long l = ceilLong(max);
+            boolean flag = (l & 3L) != l;
+            long l1 = flag ? l & 3L | 4L : l;
+            long l2 = pack(d / (double) l) << 3;
+            long l3 = pack(d1 / (double) l) << 18;
+            long l4 = pack(d2 / (double) l) << 33;
+            long l5 = l1 | l2 | l3 | l4;
+            buffer.writeByte((byte) ((int) l5));
+            buffer.writeByte((byte) ((int) (l5 >> 8)));
+            buffer.writeInt((int) (l5 >> 16));
+            if (flag) {
+                ByteBufUtil.writeVarInt((int) (l >> 2), buffer);
+            }
+        }
+    }
+
+    public static long ceilLong(double value) {
+        long l = (long) value;
+        return value > (double) l ? l + 1L : l;
+    }
+
+    public static double absMax(double x, double y) {
+        return Math.max(Math.abs(x), Math.abs(y));
+    }
+
+    private static double sanitize(double value) {
+        return Double.isNaN(value) ? (double) 0.0F : clamp(value, -1.7179869183E10, 1.7179869183E10);
+    }
+
+    private static long pack(double coord) {
+        return Math.round((coord * (double) 0.5F + (double) 0.5F) * (double) 32766.0F);
     }
 
     @Override
